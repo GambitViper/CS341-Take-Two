af9a3713d7edf2dea426551f6ba31cee1a785d1f
Zach <caday33@gmail.com>
2019-11-21 12:07:42 -0600

    2d array filled on month basis

--- DenTask/src/Classes/AppointmentCalendar.java
+++ DenTask/src/Classes/AppointmentCalendar.java
@@ -1,5 +1,6 @@
 package Classes;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -21,7 +22,10 @@ public class AppointmentCalendar {
 		System.out.println("The given calendar's month is: " + calendar.get(Calendar.MONTH));
 		System.out.println("The given calendar's day is: " + calendar.get(Calendar.DATE));
 		
-		printCalendar(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH));
+		int[][] calendarByMonth = fillArrayByMonth(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH));
+		for(int i = 0; i < calendarByMonth.length; i++) {
+			System.out.println(Arrays.toString(calendarByMonth[i]));
+		}
 	}
 
 	public static void printCalendar(int year, int month) {
@@ -79,6 +83,62 @@ public class AppointmentCalendar {
 		}
 		// This puts some space after the calendar is printed.
 		System.out.println("\n");
+
+	}
+	
+	public static int[][] fillArrayByMonth(int year, int month) {
+		int dayOfWeek = 1;
+		int[][] calendarByMonth = new int[5][7]; 
+
+		int[] calDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		// object to get month
+		Calendar cal = new GregorianCalendar(year, month, 1);
+		// Following calculates if the current year is leapyear
+		if (year % 100 == 0) {
+			if (year % 400 == 0) {
+				calDays[1] = 29;
+			}
+		} else if (year % 4 == 0) {
+			calDays[1] = 29;
+		}
+		// Following loop will find out where to print the first day of the month
+		// dayCounter represents current day of the month and dayOfWeek represents the
+		// day of the
+		// week where 1 is Sunday
+		int weekIdx = 0;
+		int dayIdx = 0;
+		for (int dayCounter = 1; dayCounter <= calDays[month]; dayCounter++) {
+			// This if will find out print out spaces until the day the first of the
+			// month falls on
+			if (dayCounter == 1) {
+				while (dayOfWeek != cal.get(Calendar.DAY_OF_WEEK)) {
+					dayIdx++;
+					dayOfWeek++;
+				}
+				// Now we have found the right place to print the first day of the month
+				calendarByMonth[weekIdx][dayIdx] = dayCounter;
+				dayIdx++;
+				dayOfWeek++;
+			} else {
+				// This checks to see if we are at the end of the row.
+				if (dayOfWeek == 8) {
+					// Print a newline and start on the next row and set the day to 2
+					// since we just printed the first position
+					weekIdx++;
+					dayIdx = 0;
+					calendarByMonth[weekIdx][dayIdx] = dayCounter;
+					dayIdx++;
+					dayOfWeek = 2;
+				} else {
+					// this prints out the current day number and increments j
+					calendarByMonth[weekIdx][dayIdx] = dayCounter;
+					dayIdx++;
+					dayOfWeek++;
+				}
+			}
+		}
+		// This puts some space after the calendar is printed.
+		return calendarByMonth;
 
 	}
 
